5c943e01455749939739fef2d2aa1df9131db49e
Neva <nevanjithdes@gmail.com>
2025-07-13 16:10:50 +0530

    first commit

---
+++ src/main/java/com/neva/invtrax/InvTraXApplication.java
@@ -0,0 +1,13 @@
+package com.neva.invtrax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvTraXApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvTraXApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/neva/invtrax/InvTraXApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neva.invtrax;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvTraXApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
